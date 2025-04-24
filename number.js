3cd71e2c31f697c9d8d8287b3a19370cb15ac839
BimaFdilana <bfadilana@gmail.com>
2025-04-25 02:40:10 +0700

    number

---
+++ number.js
@@ -0,0 +1,64 @@
+// Number adalah Angka
+// Arithmetic Operator adalah operator yang digunakan untuk melakukan operasi matematika
+
+// Penjumlahan ( + )
+const a = 10;
+const b = 5;
+const c = a + b;
+console.log(c)
+
+// Pengurangan ( - )
+const e = 5;
+const d = 10;
+const f = d - e;
+console.log(f)
+
+// Perkalian ( * )
+const g = 10;
+const h = 5;
+const i = g * h;
+console.log(i)
+
+// Pembagian ( / )
+const j = 10;
+const k = 5;
+const l = j / k;
+console.log(l)
+
+// Modulus ( % )
+// modulus adalah sisa dari pembagian 
+// ex = 5 % 2 = 1, 3 % 2 = 1, 4 % 2 = 0
+const m = 5;
+const n = 2;
+const o = m % n;
+console.log(o)
+
+// operator precedence
+// operator precedence adalah urutan operator yang digunakan dalam operasi matematika -  
+// - yang mana prioritas operator yang lebih tinggi akan dieksekusi terlebih dahulu
+// urutan operator precedence adalah sebagai berikut:
+// 1. ()
+// 2. **
+// 3. *, /, %
+// 4. +, -
+// contoh operator precedence
+const p = 10 + 10 * 10;
+const q = (10 + 10) * 10;
+console.log(p, q)
+
+// Floating Point Number
+// Floating Point Number adalah angka desimal
+// ( . ) adalah pemisah antara angka bulat dan angka desimal
+const r = 10.5;
+console.log(r)
+
+// Latihan 1 
+const apple = 5000;
+const pisang = 10000;
+const customersApple = 3;
+const customersPisang = 2;
+const kuppon = 10000;
+const total = (apple * customersApple) + (pisang * customersPisang) - kuppon;
+console.log(total)
+
+
--- string.js
+++ string.js
@@ -1,3 +1,5 @@
+// Sting adalah Text
+
 // log berfungsi untuk print ke terminal / ke console web browser
 // String bisa menggunakan tanda petik satu (' ') atau tanda petik dua (" ")
 // console.log berfungsi untuk menampilkan string ke terminal
@@ -11,17 +13,17 @@ console.log('Bima Fadi Lana')
 const myName = 'Bima Fadi Lana';
 console.log(myName)
 
-// // const adalah variable yang tidak bisa diubah
-// // kita tidak bisa mengubah errorMyName ketika menggunakan const 
-// const constMyName = 'Bima Fadi Lana';
-// constMyName = 'Bima Batu Gana';
-// console.log(constMyName)
+// const adalah variable yang tidak bisa diubah
+// kita tidak bisa mengubah errorMyName ketika menggunakan const 
+const constMyName = 'Bima Fadi Lana';
+constMyName = 'Bima Batu Gana';
+console.log(constMyName)
 
-// // let adalah variable yang bisa diubah
-// // kita bisa mengubah letMyName ketika menggunakan let
-// let letMyName = 'Bima Fadi Lana';
-// letMyName = 'Bima Batu Batuan';
-// console.log(letMyName)
+// let adalah variable yang bisa diubah
+// kita bisa mengubah letMyName ketika menggunakan let
+let letMyName = 'Bima Fadi Lana';
+letMyName = 'Bima Batu Batuan';
+console.log(letMyName)
 
 
 // Latihan 1
