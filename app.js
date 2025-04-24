0713d331c210b1145123ba6cc83225ba9bffcfa9
BimaFdilana <bfadilana@gmail.com>
2025-04-25 01:35:38 +0700

    push

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-'Bima Fadi Lana';
+// log berfungsi untuk print ke terminal / ke console web browser
+// String bisa menggunakan tanda petik satu (' ') atau tanda petik dua (" ")
+// console.log berfungsi untuk menampilkan string ke terminal
+console.log('Bima Fadi Lana')
+
+
+// variable ( const, let, var )
+// variable berfungsi untuk menyimpan data untuk menghindari repetisi
+const myName = 'Bima Fadi Lana';
+
+
+
